fe558b90eefccfdf6b547dbf44bf3dba1e75ca9d
haneug <hagen@thch.uni-bonn.de>
2023-12-02 07:59:06 +0100

    solved the second problem of day2

--- day2/script.py
+++ day2/script.py
@@ -26,6 +26,27 @@ def check_game(game: str) -> int:
 
     return int(game.split()[1].split(':')[0])
 
+def check_bag(game: str) -> int:
+    """ Return power of required bag in a game """
+
+    reveals = game.split(':')[1].split(';')
+    req_bag = {key: 0 for key in bag}
+    for reveal in reveals:
+        virt_bag = {key: 0 for key in bag}
+        rev_list = reveal.split(',')
+        for rev in rev_list:
+            number = int(rev.split()[0])
+            color = rev.split()[1]
+            virt_bag[color] += number
+        for key in req_bag:
+            if key in virt_bag and virt_bag[key] > req_bag[key]:
+                req_bag[key] = virt_bag[key]
+
+    power = 1
+    for key in req_bag:
+        power *= req_bag[key]
+
+    return power 
 
 bag = {
     'red': 12,
@@ -34,6 +55,10 @@ bag = {
 }
 
 sum_id = 0
-for game in data: sum_id += check_game(game)
+sum_power = 0
+for game in data: 
+    sum_id += check_game(game)
+    sum_power += check_bag(game)
 
 print(f'The sum of the valid game IDs is {sum_id}.')
+print(f'The sum of the powers of required bags is {sum_power}.')
