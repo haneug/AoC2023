cb049347e2b55513fb602e2b7aa9a5892f2c5c12
haneug <hagen@thch.uni-bonn.de>
2023-12-11 19:57:54 +0100

    da9 part2

--- day9/script.py
+++ day9/script.py
@@ -11,21 +11,27 @@ with open(os.path.join(workdir, inputfile)) as inp:
 
 def get_extra(line: list) -> list:
     diff = []
-    #print(line)
     for i in range(0, len(line)-1):
         diff.append(int(line[i+1])-int(line[i]))
     return diff
 
 sum_history = 0
+sum_prev_history = 0
 
 for line in data:
     list = [[int(item) for item in line.split()]]
     while any(item != 0 for item in list[-1]):
         list.append(get_extra(list[-1]))
     history = 0
+    prev_history = 0
     for item in list:
         history += item[-1]
+    rev_list = list[::-1]
+    for i in range(0, len(rev_list)-1):
+        prev_history = rev_list[i+1][0] - prev_history  
     sum_history += history
+    sum_prev_history += prev_history
 
 print(f'The sum of all histories is: {sum_history}.')
+print(f'The sum of all the previous histories is: {sum_prev_history}.')
 
